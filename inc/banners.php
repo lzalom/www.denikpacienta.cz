b680c998cc91bf7b04033b7f5d301412e03dc3bf
Lubos Zalom <lubos.zalom@gmail.com>
2014-09-04 13:07:24 +0200

    banner json

--- inc/banners.php
+++ inc/banners.php
@@ -13,7 +13,7 @@ mysql_query("set names 'utf8'");
     $x = mysql_query("SELECT * FROM banners WHERE active = 1 AND UNIX_TIMESTAMP(start) <= '".$today_time."' AND UNIX_TIMESTAMP(end) >= '".$today_time."' ORDER BY sortable ASC");
     while ($y = mysql_fetch_array($x)){
       //$banners = $banners.'{ "file1": "'.$y['file1'].'", "file2": "'.$y['file2'].'", "url": "http://'.$y['url'].'", "duration": '.$y['duration'].'},';  
-      $banners = $banners.'{ "file1": "'.$y['file1'].'", "file2": "'.$y['file2'].'", "url": "http://www.denikpacienta.cz/redirect.php?banner='.$y['id'].'", "duration": '.$y['duration'].'},';  
+      $banners = $banners.'{ "file1": "'.$y['file1'].'", "file2": "'.$y['file2'].'", "file3": "'.$y['file3'].'", "file4": "'.$y['file4'].'", "url": "http://www.denikpacienta.cz/redirect.php?banner='.$y['id'].'", "duration": '.$y['duration'].'},';  
     }
     $banners = substr($banners, 0, -1);
     echo '[ '.$banners.' ]';
